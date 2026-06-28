bb2e11b53a74473c90da03a13e7023de2788a2c6
mohamedaly0206 <mohamedaly0206@gmail.com>
2026-06-29 02:33:31 +0300

    feat: integrate Firebase services and update dependencies

--- lib/config/base_event/base_event.dart
+++ lib/config/base_event/base_event.dart
@@ -1,14 +1,17 @@
 sealed class BaseEvent {
   const BaseEvent();
 }
+
 class DisplayError extends BaseEvent {
   final String message;
   const DisplayError(this.message);
 }
+
 class DisplaySuccess extends BaseEvent {
   final String message;
   const DisplaySuccess(this.message);
 }
+
 class NavigateEvent extends BaseEvent {
   final String routeName;
   const NavigateEvent(this.routeName);
