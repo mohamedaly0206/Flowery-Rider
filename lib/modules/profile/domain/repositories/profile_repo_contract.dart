ed050816c60dd0b23d82ae41fbe6b780b59775e6
Khalid Adel <khalidadel209@gmail.com>
2026-08-10 13:28:08 +0300

    add change password method to profile repo

--- lib/modules/profile/domain/repositories/profile_repo_contract.dart
+++ lib/modules/profile/domain/repositories/profile_repo_contract.dart
@@ -2,9 +2,15 @@ import 'package:flowery_rider/config/base_response/base_response.dart';
 import 'package:flowery_rider/modules/profile/data/models/edit_profile_request_model.dart';
 import 'package:flowery_rider/modules/profile/domain/entities/driver_profile_entity.dart';
 
+import '../../data/models/change_password_request.dart';
+import '../entities/change_password_response_entity.dart';
+
 abstract interface class ProfileRepoContract {
   Future<BaseResponse<DriverProfileEntity>> getLoggedDriverData();
   Future<BaseResponse<DriverProfileEntity>> editProfile(
     EditProfileRequestModel request,
   );
+  Future<BaseResponse<ChangePasswordResponseEntity>> changePassword(
+      ChangePasswordRequest changePasswordRequest,
+      );
 }
